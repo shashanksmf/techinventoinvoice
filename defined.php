2c17da22a5d9b704f4f8a84276f8cc080d4fe54a
shashank <shashanksmf@outlook.com>
2018-04-11 13:40:17 +0530

    defined varaiables added

---
+++ defined.php
@@ -0,0 +1,19 @@
+<?php
+defined('_PATH') or die('Restricted!');
+
+define('VERSION', '1.3.0a');
+define('HTTP_SERVER', "http://anyinvoice.herokuapp.com");
+define('HTTP_APPLICATION', "http://anyinvoice.herokuapp.com");
+define('HTTPS_SERVER', "https://anyinvoice.herokuapp.com");
+define('HTTPS_APPLICATION', "https://anyinvoice.herokuapp.com");
+define('DIR_APPLICATION', "https://anyinvoice.herokuapp.com" . '/');
+define('DIR_EXTENSION', "https://anyinvoice.herokuapp.com" . '/extension/');
+define('DIR_IMAGE', "https://anyinvoice.herokuapp.com" . '/image/');
+define('DIR_LANGUAGE', "https://anyinvoice.herokuapp.com" . '/language/');
+define('DIR_SYSTEM', "https://anyinvoice.herokuapp.com" . '/system/');
+define('DIR_TEMPLATE', "https://anyinvoice.herokuapp.com" . '/view/');
+define('DB_DRIVER', '');
+define('DB_HOSTNAME', 'us-cdbr-iron-east-05.cleardb.net');
+define('DB_USERNAME', 'bcada21bc58477');
+define('DB_PASSWORD', '	96a24738');
+define('DB_DATABASE', 'heroku_51173485efddc6a');
