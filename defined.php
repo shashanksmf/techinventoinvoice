083ecd7e7b6b9f1f1511d23d2c5048bf8018f56f
shashank <shashanksmf@outlook.com>
2018-04-12 22:51:00 +0530

    definations changed according to scalingo

--- defined.php
+++ defined.php
@@ -2,18 +2,26 @@
 defined('_PATH') or die('Restricted!');
 
 define('VERSION', '1.3.0a');
-define('HTTP_SERVER', "https://anyinvoice.herokuapp.com");
-define('HTTP_APPLICATION', "https://anyinvoice.herokuapp.com");
-define('HTTPS_SERVER', "https://anyinvoice.herokuapp.com");
-define('HTTPS_APPLICATION', "https://anyinvoice.herokuapp.com");
-define('DIR_APPLICATION', "https://anyinvoice.herokuapp.com" . '/');
-define('DIR_EXTENSION', "https://anyinvoice.herokuapp.com" . '/extension/');
-define('DIR_IMAGE', "https://anyinvoice.herokuapp.com" . '/image/');
-define('DIR_LANGUAGE', "https://anyinvoice.herokuapp.com" . '/language/');
-define('DIR_SYSTEM', "https://anyinvoice.herokuapp.com" . '/system/');
-define('DIR_TEMPLATE', "https://anyinvoice.herokuapp.com" . '/view/');
+define('HTTP_SERVER', "https://easyinvoice.scalingo.io");
+define('HTTP_APPLICATION', "https://easyinvoice.scalingo.io");
+define('HTTPS_SERVER', "https://easyinvoice.scalingo.io");
+define('HTTPS_APPLICATION', "https://easyinvoice.scalingo.io");
+define('DIR_APPLICATION', "https://easyinvoice.scalingo.io" . '/');
+define('DIR_EXTENSION', "https://easyinvoice.scalingo.io" . '/extension/');
+define('DIR_IMAGE', "https://easyinvoice.scalingo.io" . '/image/');
+define('DIR_LANGUAGE', "https://easyinvoice.scalingo.io" . '/language/');
+define('DIR_SYSTEM', "https://easyinvoice.scalingo.io" . '/system/');
+define('DIR_TEMPLATE', "https://easyinvoice.scalingo.io" . '/view/');
 define('DB_DRIVER', '');
-define('DB_HOSTNAME', 'us-cdbr-iron-east-05.cleardb.net');
-define('DB_USERNAME', 'bcada21bc58477');
-define('DB_PASSWORD', '96a24738');
-define('DB_DATABASE', 'heroku_51173485efddc6a');
+
+define('DB_HOSTNAME', 'fb1cdd50-5bd5-4c69-9222-37aa532835c7.easyinvoice-5720.mysql.dbs.scalingo.com');
+define('DB_USERNAME', 'easyinvoicedbuser');
+define('DB_PASSWORD', 'easyinvoicedbuser');
+define('DB_DATABASE', 'easyinvoice_5720');
+
+
+
+// define('DB_HOSTNAME', 'us-cdbr-iron-east-05.cleardb.net');
+// define('DB_USERNAME', 'bcada21bc58477');
+// define('DB_PASSWORD', '96a24738');
+// define('DB_DATABASE', 'heroku_51173485efddc6a');
